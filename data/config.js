f72d200bb30aa30cfda83c3345ed1cdde2cb4230
hitotonazo <yamamotojumpei66@gmail.com>
2026-05-28 18:15:11 +0900

    Update config.js

--- data/config.js
+++ data/config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-  shareUrl: "https://x.com/arg_observerx?s=21&t=n9hS9eUFPNMQIQ1S4aDaOw",
+  shareUrl: "https://x.com/ARG_ObserverX/status/2059922626609570258",
   shareText: "このサイトの真相、気づいた人いますか\n普通のサイトじゃないです\n\n#おかしなサイト"
 };
